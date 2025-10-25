0dc5ca699c9f948884db1fe0114664fa11c9739e
Alexander Paolini <30964205+alexanderpaolini@users.noreply.github.com>
2025-10-25 03:17:06 -0400

    chore: clean up base

---
+++ src/server/api/routers/hello.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const helloRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  helloSecret: protectedProcedure.query(() => {
+    return "Hello (secretly)!";
+  }),
+});
